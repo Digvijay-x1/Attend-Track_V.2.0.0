31470c6fcf7c6b5dc99fc599bb55148eb5d6043f
Digvijay-x1 <hackerearthx1@gmail.com>
2025-06-22 18:27:29 +0530

    increase the req limit to 10mb

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(
 )
 
 app.use(flash());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true , limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
